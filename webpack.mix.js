aad0e1691dd77c08d507e68c1af4fa0d29265e09
Edgel Santos <edgel.santos@gmail.com>
2022-12-18 18:28:49 +0800

    add template

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/init.js', 'public/js')
+    .copy('resources/js/slick.min.js', 'public/js')
+     .copy('resources/js/jquery.nice-select.min.js', 'public/js')
+    .copy('resources/js/jquery.scrollUp.min.js', 'public/js')
+    .copy('resources/js/wow.min.js', 'public/js')
+    .copy('resources/js/jquery.ajaxchimp.min.js', 'public/js')
+    .copy('resources/fonts', 'public/fonts')
+    // .copu('resources/js/slick.min.js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/style.scss', 'public/css');
